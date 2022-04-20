69addecccc1b9fa0714b1765f418e405204bdf8d
joserogu <josemanuel.rodriguez.guerrero.alu@iesfernandoaguilar.es>
2022-04-20 09:26:04 +0200

    Realizo validaciones mediante Model Form

--- source/joserogu/forms.py
+++ source/joserogu/forms.py
@@ -8,6 +8,19 @@ class RegModelForm(forms.ModelForm):
         model = Registrado
         fields = ["nombre", "email"]
 
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        email_base, proveedor = email.split("@")
+        dominio, extension = proveedor.split(".")
+        if not extension == "com":
+            raise forms.ValidationError("Por favor utilice un email que tenga la extension .COM")
+        return email
+    
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get("nombre")
+        #validaciones
+        return nombre
+
 class RegForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     email = forms.EmailField()
